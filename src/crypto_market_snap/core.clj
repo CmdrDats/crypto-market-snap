41f9dd2a618b25e26d219a75e5b022cbb1d7ed35
Deon Moolman <deon@yuppiechef.com>
2017-10-09 15:51:08 +0200

    new project

---
+++ src/crypto_market_snap/core.clj
@@ -0,0 +1,6 @@
+(ns crypto-market-snap.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/crypto_market_snap/core_test.clj
@@ -0,0 +1,7 @@
+(ns crypto-market-snap.core-test
+  (:require [clojure.test :refer :all]
+            [crypto-market-snap.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
